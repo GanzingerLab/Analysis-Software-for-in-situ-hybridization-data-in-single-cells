2baaf4c1f843dbab76181f252bcaa5f6d9d8d44a
kganzinger <kganzinger@users.noreply.github.com>
2017-12-12 19:01:27 +0100

    delete unused functions

--- Code/Versions-used-for-analysis-in-Ranasinghe-et-al/three-colour-data/source codes/unused code/compare_GroundTruth.m
+++
@@ -1,9 +0,0 @@
-function compare_GroundTruth(minLength, name4window)
-load groundTruth.mat;
-nTracks = length(data);
-for n=1:nTracks,
-    myTrack = [n*ones(length(data{n}.width), 1), data{n}.mu(:, 2), data{n}.mu(:, 1), data{n}.width', data{n}.amplitude'];
-    if size(myTrack, 1)>minLength,
-        drawTest(myTrack, n, name4window);
-    end
-end
--- Code/Versions-used-for-analysis-in-Ranasinghe-et-al/three-colour-data/source codes/unused code/normalise_image.m
+++
@@ -1,6 +0,0 @@
-function I_out = normalise_image(I)
-%finds the global min/maximum and normalises the movie
-
-global_max=max(max(max(I)));
-
-I_out=(I)./(global_max);
